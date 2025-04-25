620f99213d5fe8b0a16f66e2203575a67d084f84
Framemanee <64011212081@msu.ac.th>
2025-04-25 11:47:05 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills";
+import { Skills_1 } from "./components/Skills_1";
+import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      
+      <Skills />
+      <Skills_1 />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
